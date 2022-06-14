d472652b755f49d91460eda5b24a2b5ded39196b
Kolodiazhnyi Serhii <elkrot@gmail.com>
2022-06-14 13:20:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 type Arguments map[string]string
@@ -51,33 +52,102 @@ func Perform(args Arguments, writer io.Writer) error {
 			return fmt.Errorf(err.Error())
 		}
 	case "remove":
-		err = remove(args["is"], writer)
+		err = remove(fileName,args["id"], writer)
 		if err != nil {
 			return fmt.Errorf(err.Error())
 		}
 	case "findById":
-		err = findById(args["id"], writer)
+		err = findById(fileName,args["id"], writer)
 		if err != nil {
 			return fmt.Errorf(err.Error())
 		}
 	default:
 		return fmt.Errorf("Operation %s not allowed!", operation)
-	}
-
-	_, errf := os.Open(fileName)
-	if errf != nil {
-		return fmt.Errorf("!")
-	}
+	}	
 
 	return nil
 }
 
-func findById(s string, writer io.Writer) error {
-	panic("unimplemented")
+func findById(fileName string,id string, writer io.Writer) error {
+	if id == "" {
+		return fmt.Errorf("-id flag has to be specified")
+	}
+
+    var filePermission fs.FileMode = 0644
+
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, filePermission)
+	if err != nil {
+		return err
+	}
+    items,err:=getAllItems(file)
+
+	if err != nil {
+		return err
+	}	
+
+	for _, item := range items {
+		if id == item.Id {
+
+			bt, err := json.Marshal(item)
+			if err != nil {
+				
+				return err
+			}	
+			val:=strings.ToLower(string(bt))
+			writer.Write([]byte(val))
+			file.Close()
+			
+			return nil
+		}
+	}
+	
+	file.Close()	
+
+	return nil
 }
 
-func remove(s string, writer io.Writer) error {
-	panic("unimplemented")
+func remove(fileName string,id string, writer io.Writer) error {
+
+	if id == "" {
+			return fmt.Errorf("-id flag has to be specified")
+	}
+	var filePermission fs.FileMode = 0644
+
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, filePermission)
+	if err != nil {
+		return err
+	}
+    items,err:=getAllItems(file)
+
+	if err != nil {
+		return err
+	}	
+	file.Close()
+
+	for i := 0; i < (len(items) - 1); {
+		if id == items[i].Id {
+            items=removeItemByIndex(items, i)
+			bt, err := json.Marshal(items)
+			if err != nil {				
+				return err
+		}
+
+		val:=strings.ToLower(string(bt))
+		writer.Write([]byte(val))
+		file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, filePermission)
+		if err != nil {
+			return err
+		}
+		file.Write([]byte(val))
+		file.Close()			
+		return nil
+		} 
+	}
+	
+	file.Close()
+	writer.Write([]byte(fmt.Sprintf("Item with id %s not found",id)))
+
+	return nil
 }
 
 func add(fileName string, strItem string, writer io.Writer) error {
@@ -85,48 +155,43 @@ func add(fileName string, strItem string, writer io.Writer) error {
 	b := []byte(strItem)
 	var item Item
 	err := json.Unmarshal(b, &item)
+
 	if err != nil {
 		return err
 	}
 
-	var filePermission fs.FileMode = 0644
+    var filePermission fs.FileMode = 0644
 	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, filePermission)
 
 	if err != nil {
 		return err
 	}
-
-	bytes, err := ioutil.ReadAll(file)
+	items,err:=getAllItems(file)
 	if err != nil {
 		return err
-	}
+	}	
 
-	var items []Item
-	err = json.Unmarshal(bytes, &items)
-	if err != nil {
-		return err
-	}
 	for _, i := range items {
-
 		if fmt.Sprint(item.Id) == i.Id {
 
-			writer.Write([]byte("Item with id 1 already exists"))
+			writer.Write([]byte(fmt.Sprintf("Item with id %s already exists",i.Id)))
 			file.Close()
 			return nil
 		}
-	}
+	}	
 
 	items = append(items, item)
+
 	bt, err := json.Marshal(items)
-	if err != nil {
-		fmt.Println(items)
+	if err != nil {		
 		return err
 	}
 
-	file.Write([]byte(bt))
+	val:=strings.ToLower(string(bt))
+	file.Write([]byte(val))
 	file.Close()
 
-	writer.Write(bt)
+	writer.Write([]byte(val))
 
 	return nil
 }
@@ -167,6 +232,26 @@ func parseArgs() Arguments {
 		"fileName":  *fileName,
 	}
 	return args
+}
+
+func getAllItems(file *os.File) ([]Item,error){
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil,err
+	}
+	var items []Item
+	if len(bytes)!=0 {
+		err = json.Unmarshal(bytes, &items)
+	if err != nil {
+		return nil,err
+	}
+		return items,nil
+    }
+   return nil,nil
+}
+
+func removeItemByIndex(s []Item, index int) []Item {
+	return append(s[:index], s[index+1:]...)
 }
 
 func main() {
